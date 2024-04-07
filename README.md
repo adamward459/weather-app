15424bb0c24c8fe5b1847db3ddb8683bb1f821ce
Phạm Thành Trung <adamward459@gmail.com>
2024-04-07 09:38:47 +0700

    Update UI styles, fix bugs, refactor components, and add screenshots to README.md

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-# Installation
+# Project Name
+
+This project is a weather application built with React and TypeScript. It uses OpenWeatherMap API to fetch weather data.
+
+### Project Structure
+```sh
+.
+├── README.md
+├── coverage
+├── index.html
+├── node_modules
+├── package-lock.json
+├── package.json
+├── postcss.config.js
+├── public
+├── screenshots
+├── src
+├── tailwind.config.js
+├── tsconfig.json
+├── tsconfig.node.json
+└── vite.config.ts
+
+6 directories, 9 files
+
+```
+
+### Installation
 
 1. Clone this project
 
@@ -27,3 +53,15 @@ npm run test
 
 ![alt text](./screenshots/home-page.png)
 ![alt text](./screenshots/search-page.png)
+
+### Navigation between pages
+
+Clicking on `Location` icon should navigate to Home page
+
+Clicking on `Search` icon should navigate to Search page.
+
+![alt text](./screenshots/instruction.png)
+
+### License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -9,24 +9,33 @@ export default function RootPage() {
   const navigate = useNavigate();
   const selectedGeo = useAtomValue(selectedGeoAtom);
 
-  const onInputClick = useCallback(() => {
+  const onSearchIconClick = useCallback(() => {
     navigate('/search');
+  }, [navigate]);
+
+  const onLocationIconClick = useCallback(() => {
+    navigate('/');
   }, [navigate]);
 
   return (
     <div className="min-h-screen bg-blue-100">
       <nav className="bg-white">
         <label className="input input-bordered mx-auto flex w-1/3 min-w-[300px] items-center gap-2 border-none bg-white px-0">
-          <IconLocation className="fill-black" />
+          <IconLocation
+            className="cursor-pointer fill-black"
+            onClick={onLocationIconClick}
+          />
           <input
+            disabled
             value={selectedGeo?.name}
             type="text"
             className="grow text-black"
             placeholder="Search"
-            onClick={onInputClick}
-            onChange={() => {}}
           />
-          <IconSearch className="fill-black" />
+          <IconSearch
+            className="cursor-pointer fill-black"
+            onClick={onSearchIconClick}
+          />
         </label>
       </nav>
       <div className="mt-5">
